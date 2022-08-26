9159a27e3cf3022c22a7125b52d75137f25ad3c6
Ahmet <akoc@hostmycalls.com>
2022-08-26 12:30:16 +0300

    first push

---
+++ app.js
@@ -0,0 +1,35 @@
+window.onload = function () {
+  var token;
+
+  var requestURL = document.URL;
+
+  var queryString = document.URL.substring(requestURL.indexOf("?") + 1, requestURL.length);
+
+  var params = queryString.split("&");
+
+  for (var i = 0; i < params.length; i++) {
+    var name = params[i].substring(0, params[i].indexOf("="));
+    var value = params[i].substring(params[i].indexOf("=") + 1, params[i].length);
+
+    if (isNaN(parseInt(value))) {
+      params[i] = params[i].replace(value, "'" + value + "'");
+    }
+
+    eval(params[i]);
+  }
+
+  var ifrm = document.createElement("iframe");
+
+  var url = "http://amokachi.hostmycalls.com:1903/?token=" + token;
+
+  ifrm.setAttribute("src", url);
+
+  ifrm.style.position = "fixed";
+  ifrm.style.width = "100%";
+  ifrm.style.height = "100%";
+  ifrm.style.bottom = "0";
+  ifrm.style.right = "0";
+  ifrm.style.overflow = "hidden";
+
+  document.body.appendChild(ifrm);
+};
