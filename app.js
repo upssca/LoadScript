8466300cb59534aff0e0070ce6afdfafb5809a86
Ahmet <akoc@hostmycalls.com>
2022-08-26 20:29:58 +0300

    logs are removed

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 window.onload = function () {
   var token;
 
-  var requestURL = document.getElementById("HMCWidget").getAttribute("src");
+  var requestURL = document.getElementById("HMCWidgetIFrame").getAttribute("src");
   var queryString = requestURL.substring(requestURL.indexOf("?") + 1, requestURL.length);
-  console.log(queryString);
   var params = queryString.split("&");
-  console.log(params);
   for (var i = 0; i < params.length; i++) {
     var name = params[i].substring(0, params[i].indexOf("="));
     var value = params[i].substring(params[i].indexOf("=") + 1, params[i].length);
@@ -15,9 +13,8 @@ window.onload = function () {
     }
     eval(params[i]);
   }
-  var ifrm = document.createElement("HMCWidgetIFrame");
+  var ifrm = document.createElement("iframe");
   var url = "http://amokachi.hostmycalls.com:1903/?token=" + token;
-  console.log(url);
 
   ifrm.setAttribute("src", url);
 
