069224046390688d0436429352c684bcdb37e24d
Ahmet <akoc@hostmycalls.com>
2022-08-26 16:57:24 +0300

    change

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 window.onload = function () {
   var token;
 
-  var requestURL = request.URL;
+  var requestURL = document.getElementById("myScript").getAttribute("src");
   console.log(requestURL);
   var queryString = requestURL.substring(requestURL.indexOf("?") + 1, requestURL.length);
   console.log(queryString);
   var params = queryString.split("&");
-  console.log(params);
   for (var i = 0; i < params.length; i++) {
     var name = params[i].substring(0, params[i].indexOf("="));
     var value = params[i].substring(params[i].indexOf("=") + 1, params[i].length);
@@ -14,7 +13,6 @@ window.onload = function () {
     if (isNaN(parseInt(value))) {
       params[i] = params[i].replace(value, "'" + value + "'");
     }
-    console.log(eval(params[i]));
     eval(params[i]);
   }
   var ifrm = document.createElement("iframe");
