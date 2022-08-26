9cb687b746d699ad57cd85421e11aeaacc43e1d6
Ahmet <akoc@hostmycalls.com>
2022-08-26 20:17:59 +0300

    log

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ window.onload = function () {
 
   var requestURL = document.getElementById("HMCWidget").getAttribute("src");
   var queryString = requestURL.substring(requestURL.indexOf("?") + 1, requestURL.length);
+  console.log(queryString);
   var params = queryString.split("&");
+  console.log(params);
   for (var i = 0; i < params.length; i++) {
     var name = params[i].substring(0, params[i].indexOf("="));
     var value = params[i].substring(params[i].indexOf("=") + 1, params[i].length);
@@ -13,6 +15,7 @@ window.onload = function () {
     }
     eval(params[i]);
   }
+  console.log(url);
   var ifrm = document.createElement("HMCWidgetIFrame");
   var url = "http://amokachi.hostmycalls.com:1903/?token=" + token;
   ifrm.setAttribute("src", url);
